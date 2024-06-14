2603d9167bbbf229097d806191e4ea6846a5c4a2
Marcin Rybkowski <marcin.rybkowski@enigma.com.pl>
2024-06-14 15:18:10 +0200

    MC-3 | Przygotowanie templatki react typescript dla aplikacji portal i client

---
+++ frontend/portal/src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <h1>Portal</h1>
+    );
+}
+
+export default Home;
---
+++ frontend/portal/src/pages/notFound/NotFound.tsx
@@ -0,0 +1,7 @@
+function NotFound() {
+    return (
+        <h1>Not Found Component</h1>
+    );
+}
+
+export default NotFound;
